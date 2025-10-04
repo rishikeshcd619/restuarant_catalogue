29d696f146073a18054ed8230354ccde34540316
cd Rishii <rishikeshcd619@gmail.com>
2025-10-04 20:25:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
-import "./App.css";
-
-const Header = () => {
-  return (
-    <div className="header">
-      <img
-        className="logo"
-        src="https://images-platform.99static.com//dHKNZHpjCOlNh2mM59Cy8kcjDJ0=/0x0:960x960/fit-in/500x500/99designs-contests-attachments/127/127747/attachment_127747557"
-      />
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About us</li>
-          <li>Contact us</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-const Body = () => {
-  return (
-    <div className="body">
-      <div className="search">Search</div>
-      <div className="restuarant-container">Container</div>
-    </div>
-  );
-};
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import { UserContext } from "./utils/UserContext";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 const App = () => {
+  const user = {
+    name: "Rishi",
+  };
   return (
     <div className="App">
-      <Header />
-      <Body />
+      <Provider store={appStore}>
+        <UserContext.Provider value={user}>
+          <Header />
+          <Outlet />
+        </UserContext.Provider>
+      </Provider>
     </div>
   );
 };
